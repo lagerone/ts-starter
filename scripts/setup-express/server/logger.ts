0b8128980f2a36d8a4dc62079ad9c4b6825dab03
Daniel Lager <daniel.lager@tretton37.com>
2019-06-17 09:44:06 +0200

    Fix annoying stuff

---
+++ scripts/setup-express/server/logger.ts
@@ -0,0 +1,16 @@
+import winston from 'winston';
+
+const logger = winston.createLogger({
+  level: process.env.LOG_LEVEL || 'debug',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      ),
+    }),
+  ],
+});
+
+export { logger };
--- scripts/setup-express/server/server.ts
+++ scripts/setup-express/server/server.ts
@@ -1,21 +1,22 @@
 require('dotenv').config({ silent: true });
+
 import errorHandler from 'errorhandler';
 import app from './app';
-import log from './log';
+import { logger } from './logger';
 
 app.use(errorHandler());
 
 const server = app.listen(app.get('port'), () => {
-  log.debug(
+  logger.debug(
     `App is running at http://localhost:${app.get('port')} in ${app.get(
       'env'
     )} mode`
   );
-  log.debug('Press CTRL-C to stop');
+  logger.debug('Press CTRL-C to stop');
 });
 
 process.on('SIGUSR2', () => {
-  log.debug('  Bye bye');
+  logger.debug('Bye bye.');
   process.exit(0);
 });
 
